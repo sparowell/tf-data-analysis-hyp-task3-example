af8faf262785f93d12fec4d88d20e0d19b2e4415
sparowell <103679474+sparowell@users.noreply.github.com>
2023-04-12 21:41:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from hyppo.ksample import MMD
 
 chat_id = 654929803 # Ваш chat ID, не меняйте название переменной
 
 def solution(x,y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     #p = kruskal(x,y).pvalue
-    p = ks_2samp(x,y,alternative='lower').pvalue
+    #p = ks_2samp(x,y,alternative='lower').pvalue
+    p = MMD(compute_kernel="rbf", gamma=10).test(x, y)[1]
     return p<0.05
