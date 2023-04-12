f48f761eed285bedbbe644e7e9837e4cc8be8133
sparowell <103679474+sparowell@users.noreply.github.com>
2023-04-12 12:02:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import kruskal
 
+chat_id = 654929803 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x,y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    p = kruskal(x,y).pvalue
+    return p<0.05
